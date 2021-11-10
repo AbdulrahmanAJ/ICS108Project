9131e2eee856022c1947f4ed00e98f49ae770d71
Abdulrahman Jamal <aboodx@me.com>
2021-11-10 22:32:40 +0300

    fixing the main file

--- src/main/java/com/example/ics108project/Main.java
+++ src/main/java/com/example/ics108project/Main.java
@@ -5,8 +5,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
