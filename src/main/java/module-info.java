f74bdd53f4273b994a5cb7e31bfa29a4a2ebb865
Abdulrahman Jamal <aboodx@me.com>
2021-11-10 22:23:08 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.ics108project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.ics108project to javafx.fxml;
+    exports com.example.ics108project;
+}
